99c3b894392bf2198b8e4bbd91c0652b1a2f0028
lautaro3870 <lautarodisbro@gmail.com>
2023-09-02 11:26:50 +0200

    Endpoint envio mail

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
-const express = require('express');
+const express = require("express");
+const enviarCorreo = require("./mailer");
+const cron = require("node-cron");
+
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const port = process.env.PORT ?? 3000;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+cron.schedule("14 13 * * *", () => {
+  console.log("Envio programado");
+  const destinatario = "lautarodisbro@gmail.com"; // Cambiar por el correo del destinatario
+  const asunto = "Correo automático semanal";
+  const mensaje = "html2";
+  enviarCorreo(destinatario, asunto, mensaje);
+});
+
+app.get("/envioMail", function (req, res) {
+  res.send("Hello World");
+  const destinatario = "lautarodisbro@gmail.com"; // Cambiar por el correo del destinatario
+  const asunto = "Correo automático semanal";
+  const mensaje =
+    "Este es un correo automático que se envía una vez a la semana.";
+  enviarCorreo(destinatario, asunto, mensaje);
+});
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+  console.log(`App listening on port ${port}`);
+});
